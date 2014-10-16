c26e9d4c2e2788a55c3df356ae09867ea1a8e97a
43771515 <ashley.mcclenaghan@hsbcib.com>
2014-10-16 20:47:00 +0100

    Minor

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject friend-jaas "0.1.0-SNAPSHOT"
+  :description "Helps com.cemerick/friend to use JAAS."
+  :url "https://github.com/ash-mcc/friend-jaas"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [com.cemerick/friend "0.2.1"]])
