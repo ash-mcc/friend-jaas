3d4d7977f84e61fbb58184beebef74be68641ad1
43771515 <ashley.mcclenaghan@hsbcib.com>
2014-10-17 10:19:50 +0100

    Minor

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject friend-jaas "0.1.0-SNAPSHOT"
+(defproject ash-mcc/friend-jaas "0.1.0"
   :description "Helps com.cemerick/friend to use JAAS."
   :url "https://github.com/ash-mcc/friend-jaas"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.cemerick/friend "0.2.1"]])
